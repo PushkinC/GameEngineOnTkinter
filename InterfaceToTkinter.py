c94b71732c7276624c2f4a039a3ae1307a012132
PushkinC <zolotarev_d_e@mail.ru>
2022-08-31 22:42:42 +0300

    fix resistens

--- InterfaceToTkinter.py
+++ InterfaceToTkinter.py
@@ -1,15 +1,16 @@
-
+import math
 objects = []
 GRAVITY = 0.1
 
-class Oval:
+
+
+class Object:
     def __init__(self, x1, y1, x2, y2, color, canvas, isPhysics=False, collision=[], mass=1, resistens=0.01):
         if collision == []:
             collision = [x1, y1, x2, y2]
         self.velocity = {'x': 0, 'y': 0}
         self.size = [x1, y1, x2, y2]
         self.color = color
-        self.object = canvas.create_oval((x1, y1), (x2, y2), fill=color)
         self.isPhysics = isPhysics
         objects.append(self)
         self.collision = collision
@@ -21,12 +22,12 @@ class Oval:
         if self.isPhysics == True:
             self.velocity['y'] += GRAVITY
 
-
         self.velocity['x'] -= self.velocity['x'] * self.resistens
         self.velocity['y'] -= self.velocity['y'] * self.resistens
 
         self.canvas.move(self.object, self.velocity['x'], self.velocity['y'])
         print(self.velocity)
+        print(self.velocity['y'] * self.resistens)
 
     def move(self, x, y):
         self.canvas.move(self.object, x, y)
@@ -34,6 +35,7 @@ class Oval:
     def addForce(self, x, y):
         self.velocity['x'] += x / self.mass
         self.velocity['y'] += y / self.mass
+        print('asdasd')
 
     def addVelocity(self, x, y):
         self.velocity['x'] += x
@@ -43,6 +45,11 @@ class Oval:
         self.velocity['x'] = x
         self.velocity['y'] = y
 
-    # def bind(self, tk, ref, func):
-    #     tk.bind(ref, func)
+
+
+
+class Oval(Object):
+    def __init__(self, x1, y1, x2, y2, color, canvas, isPhysics=False, collision=[], mass=1, resistens=0.01):
+        super().__init__(x1, y1, x2, y2, color, canvas, isPhysics, collision, mass, resistens)
+        self.object = canvas.create_oval((x1, y1), (x2, y2), fill=color)
 
